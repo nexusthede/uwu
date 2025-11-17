8c901054191c9ba9c8c752940bf66055442bd407
nexusthede <ivanrrr76@gmail.com>
2025-11-16 23:06:33 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    prefix: '.',
+    serverId: '1426789471776542803',
+    lbChannels: {
+        chat: null,
+        voice: null,
+        points: null
+    },
+    roles: {
+        admin: null,   // set via .set admin @role
+        mod: null,
+        owner: null
+    },
+    voiceTiers: {
+        tier1: null,   // set via .set tier 1 @role
+        tier2: null,
+        tier3: null,
+        tier4: null,
+        tier5: null
+    }
+};
